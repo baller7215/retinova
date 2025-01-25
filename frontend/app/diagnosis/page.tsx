0315b12f9dbaf3751a15c6e8aa5465481e94bee5
NoNathan17 <nathanong1217@gmail.com>
2025-01-25 14:09:45 -0800

    diagnosis page implemented

--- frontend/app/diagnosis/page.tsx
+++ frontend/app/diagnosis/page.tsx
@@ -1,7 +1,76 @@
+import Image from "next/image";
+
+import { Button } from "@mui/material";
+import { BiAperture } from "react-icons/bi";
+import { BsArrowUpRightCircle } from "react-icons/bs";
+
+
 export default function Diagnosis() {
     return (
-        <div className="text-center text-7xl">
-            big d big d big d big d
-        </div>
+          <div className="md:flex grid md:flex-cols-2 md:pl-36 py-1 md:py-4 font-sans">
+              {/* Left Section: Description */}
+            <div className="flex flex-col gap-5 w-5/6">
+                <div className="flex flex-col w-11/12 md:w-5/6 text-center md:text-left text-balance mx-auto md:mx-0">
+                    <h1 className="flex text-xl md:text-2xl leading-snug font-medium mx-auto md:mx-0 gap-1">
+                        <p>Patient Imaging</p>
+                        <BiAperture className="my-auto text-3xl" />
+                    </h1>
+                    <p className="text-[#FFFFFF] mt-1 md:mt-6 text-sm md:text-base leading-relaxed font-light">
+                    Our application provides users with a quick, faster way to diagnose patients using self-taken pictures. 
+                    Start by uploading a clear, visible image of your eye. Our purpose is for patients to get a general, 
+                    but quick diagnose without having to seek out professionally taken photos first.
+                    </p>
+                </div>
+                <div className="relative bg-[url('/lamp.png')] bg-cover bg-center rounded-xl w-2/3 h-64 md:h-80 flex items-center justify-center">
+                    <p className="text-lg md:text-6xl font-extrabold absolute top-28 -left-20 rotate-90">PATIENT</p>
+                    <p className="text-lg md:text-6xl font-extrabold absolute -bottom-10 left-6">IMAGING</p>
+                    <Button
+                        href="/gyno"
+                        variant="text"
+                        sx={{
+                            color: "#FFFFFF",
+                            display: "flex",
+                            gap: "10px",
+                            alignItems: "center",
+                            fontSize: {xs:"16px", md:"18px"},
+                            transition: "transform 0.3s ease",
+                            fontWeight: "light",
+                            "&:hover": {
+                                transform: "scale(1.05)", 
+                            },
+                        }}
+                    >
+                    UPLOAD FILE
+                    <BsArrowUpRightCircle className="hover:animate-spin text-[#F9C7FF]" style={{fontSize: "26px"}}/>
+                    </Button>
+                    <hr className="absolute -bottom-6 right-5 w-1/2 border-t-4 border-[#FFFFFF] rounded-full" />
+                </div>
+                <div className="flex flex-col mt-14 gap-2 w-3/5 text-right">
+                    <div className="flex gap-10">
+                        <div className="flex flex-col w-1/5">
+                            <p className="font-semibold text-4xl">100+ </p>
+                            <p className="font-light text-lg">Photos</p>
+                        </div>
+                        <p className="w-4/5 text-left">Our imaging utilizes an AI trained model with hand-selected photos to detect uveitis and cataracts.</p>
+                    </div>
+                    <div className="flex gap-10">
+                        <div className="flex flex-col w-1/5">
+                            <p className="font-semibold text-4xl">80%</p>
+                            <p className="font-light text-lg">Confidence</p>
+                            <p className="font-light text-lg">Score</p>
+                        </div>
+                        <p className="w-4/5 text-left">With a confidence score of __%, we can lessen the amount of false diagnoses and provide patients with an accurate and quick diagnose.</p>
+                    </div>
+                </div>
+            </div>
+            {/* Right Content: Image */}
+            <Image
+                src="/eyenova.png"
+                alt="Eye Nova Image"
+                width={500} 
+                height={100}
+                className="hidden md:block md:max-h-[60vh]"
+            />
+            </div>         
     );
-}
+  }
--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -6,14 +6,14 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <footer className="absolute flex text-white font-sans bottom-1 ml-auto py-1 md:py-0 px-2 md:px-6 w-full justify-end items-center gap-2 md:gap-4">
-        <div className="flex flex-col">
-            <a href="https://www.youtube.com/watch?v=phuiiNCxRMg" target="_blank" className="text-sm md:text-xl text-[#858585] text-center">made w/ 💥</a>
-            <p className="text-sm md:text-xl text-[#858585]">irvine hacks '25</p>
+    <footer className="absolute flex text-[#858585] font-sans bottom-2 ml-auto py-1 md:py-0 px-2 md:px-6 w-full justify-end items-center gap-2 md:gap-4">
+        <div className="flex flex-col text-xs md:text-xl">
+            <a href="https://www.youtube.com/watch?v=phuiiNCxRMg" target="_blank" className="text-center">made w/ 💥</a>
+            <p>irvine hacks '25</p>
         </div>
         <div>
-        <Link href="https://github.com/baller7215/retinova" target="_blank" rel="noopener noreferrer">
-          <FaGithub className="w-5 md:w-10 h-5 md:h-10 text-[#858585] hover:text-gray-400" />
+        <Link href="https://github.com/baller7215/resinova" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="w-5 md:w-10 h-5 md:h-10 hover:text-gray-400" />
         </Link>
       </div>
     </footer>

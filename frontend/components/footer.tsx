9c488ce226fac146c235561c8058ae9802a9ba01
NoNathan17 <nathanong1217@gmail.com>
2025-01-25 11:50:16 -0800

    changed position of footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -6,7 +6,7 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <footer className="absolute flex text-white font-sans bottom-0 ml-auto py-1 md:py-0 px-2 md:px-6 w-full justify-end items-center gap-2 md:gap-4">
+    <footer className="absolute flex text-white font-sans bottom-1 ml-auto py-1 md:py-0 px-2 md:px-6 w-full justify-end items-center gap-2 md:gap-4">
         <div className="flex flex-col">
             <a href="https://www.youtube.com/watch?v=phuiiNCxRMg" target="_blank" className="text-sm md:text-xl text-[#858585] text-center">made w/ 💥</a>
             <p className="text-sm md:text-xl text-[#858585]">irvine hacks '25</p>

2ca795e326f6de2996a27e852c56ee3b5679f13f
NoNathan17 <nathanong1217@gmail.com>
2025-01-24 23:20:00 -0800

    navbar and footer

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -4,26 +4,26 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-black text-white fixed top-20 w-full px-36">
+    <nav className="bg-black fixed top-20 w-full px-36">
       <div className="flex items-center justify-between mx-auto">
         {/* Logo */}
-        <div className="text-4xl font-bold tracking-wide">
+        <div className="text-5xl text-[#FFFFFF] font-bold tracking-wide">
           <Link href="/">
-            <span className="block">Reti</span>
+            <span className="block ">Reti</span>
             <span className="block ml-10">Nova</span>
           </Link>
         </div>
 
         {/* Nav Links */}
-        <div className="flex gap-10 text-4xl">
+        <div className="flex gap-10 text-4xl text-[#858585] font-thin">
           <Link
            href="/about" 
-           className="hover:underline">
+           className="hover:underline hover:text-[#FFFFFF]">
           Overview
           </Link>
           <Link 
             href="/" 
-            className="hover:underline">
+            className="hover:underline hover:text-[#FFFFFF]">
             Home
           </Link>        
         </div>
